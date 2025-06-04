3265ce240160c75adaf971413eb6244f93aadcea
maksymromanyshak <romanysh123@gmail.com>
2025-06-04 15:53:16 +0300

    fix default display

--- public/script.js
+++ public/script.js
@@ -1,9 +1,14 @@
+let lastUpdateTimestamp = Date.now();
+const TIMEOUT_MS = 3000;
+
 async function fetchData() {
   try {
     let response = await fetch("/data");
     let data = await response.json();
     let pressure = Math.round(data.pressure);
     pressure = Math.max(pressure, 0);
+
+    lastUpdateTimestamp = Date.now();
 
     updatePressureDisplay(pressure);
   } catch (error) {
@@ -33,4 +38,12 @@ document.querySelector(".from").addEventListener("onChange", () => fetchData());
 document.querySelector(".to").addEventListener("onChange", () => fetchData());
 
 fetchData();
-setInterval(fetchData, 1000); // Fetch data every 3 seconds
+setInterval(fetchData, 1000);
+
+setInterval(() => {
+  if (Date.now() - lastUpdateTimestamp > TIMEOUT_MS) {
+    let pressureElement = document.getElementById("pressure");
+    pressureElement.textContent = "--";
+    pressureElement.style.backgroundColor = "#d9d9d9";
+  }
+}, 1000);
